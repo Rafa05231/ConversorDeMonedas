008e1f1971b0bd286daeb08c165cfcb5ab6ace53
Rafael Campo <campo0135@gmail.com>
2024-06-23 19:52:58 -0500

    Creacion de menu y consumo de la API para la conversion de monedas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,134 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner lectura = new Scanner(System.in);
+        ConsultarMoneda consultarMoneda = new ConsultarMoneda();
+        int opcion=0;
+        double cantidadConversion;
+        String monedaBase;
+        String monedaConvertida;
+        Moneda moneda;
+
+        while(opcion!=7) {
+            System.out.println("*********************************************************************************\n");
+            System.out.println("Bienvenido al sistema conversor de monedas, escoge la opcion que desees: ");
+            System.out.println("1) Dolar => Peso argentino");
+            System.out.println("2) Peso argentino => Dolar");
+            System.out.println("3) Won surcoreano => Peso colombiano");
+            System.out.println("4) Peso colombiano => Won surcoreano");
+            System.out.println("5) Dolar => Peso colombiano");
+            System.out.println("6) Peso colombiano => Dolar");
+            System.out.println("7) Salir\n");
+            System.out.println("*********************************************************************************");
+            opcion = lectura.nextInt();
+
+            switch (opcion){
+                case 1:
+                    monedaBase = "USD";
+                    monedaConvertida = "ARS";
+                    System.out.println("Ingresa la cantidad que deseas convertir: ");
+                    cantidadConversion = lectura.nextDouble();
+
+                    try{
+                        moneda = consultarMoneda.convertirMoneda(monedaBase,monedaConvertida,cantidadConversion);
+                        System.out.println("El valor de "+cantidadConversion+" "+moneda.base_code()+" equivalen a "
+                                + moneda.conversion_result()+" "+ moneda.target_code());
+                    }catch (RuntimeException e){
+                        System.out.println(e.getMessage());
+                        System.out.println("Finalizando programa!");
+                        opcion=7;
+                    }
+                    break;
+                case 2:
+                    monedaBase = "ARS";
+                    monedaConvertida = "USD";
+                    System.out.println("Ingresa la cantidad que deseas convertir: ");
+                    cantidadConversion = lectura.nextDouble();
+
+                    try{
+                        moneda = consultarMoneda.convertirMoneda(monedaBase,monedaConvertida,cantidadConversion);
+                        System.out.println("El valor de "+cantidadConversion+" "+moneda.base_code()+" equivalen a "
+                                + moneda.conversion_result()+" "+ moneda.target_code());
+                    }catch (RuntimeException e){
+                        System.out.println(e.getMessage());
+                        System.out.println("Finalizando programa!");
+                        opcion=7;
+                    }
+                    break;
+                case 3:
+                    monedaBase = "KRW";
+                    monedaConvertida = "COP";
+                    System.out.println("Ingresa la cantidad que deseas convertir: ");
+                    cantidadConversion = lectura.nextDouble();
+
+                    try{
+                        moneda = consultarMoneda.convertirMoneda(monedaBase,monedaConvertida,cantidadConversion);
+                        System.out.println("El valor de "+cantidadConversion+" "+moneda.base_code()+" equivalen a "
+                                + moneda.conversion_result()+" "+ moneda.target_code());
+                    }catch (RuntimeException e){
+                        System.out.println(e.getMessage());
+                        System.out.println("Finalizando programa!");
+                        opcion=7;
+                    }
+                    break;
+                case 4:
+                    monedaBase = "COP";
+                    monedaConvertida = "KRW";
+                    System.out.println("Ingresa la cantidad que deseas convertir: ");
+                    cantidadConversion = lectura.nextDouble();
+
+                    try{
+                        moneda = consultarMoneda.convertirMoneda(monedaBase,monedaConvertida,cantidadConversion);
+                        System.out.println("El valor de "+cantidadConversion+" "+moneda.base_code()+" equivalen a "
+                                + moneda.conversion_result()+" "+ moneda.target_code());
+                    }catch (RuntimeException e){
+                        System.out.println(e.getMessage());
+                        System.out.println("Finalizando programa!");
+                        opcion=7;
+                    }
+                    break;
+                case 5:
+                    monedaBase = "USD";
+                    monedaConvertida = "COP";
+                    System.out.println("Ingresa la cantidad que deseas convertir: ");
+                    cantidadConversion = lectura.nextDouble();
+
+                    try{
+                        moneda = consultarMoneda.convertirMoneda(monedaBase,monedaConvertida,cantidadConversion);
+                        System.out.println("El valor de "+cantidadConversion+" "+moneda.base_code()+" equivalen a "
+                                + moneda.conversion_result()+" "+ moneda.target_code());
+                    }catch (RuntimeException e){
+                        System.out.println(e.getMessage());
+                        System.out.println("Finalizando programa!");
+                        opcion=7;
+                    }
+                    break;
+                case 6:
+                    monedaBase = "COP";
+                    monedaConvertida = "USD";
+                    System.out.println("Ingresa la cantidad que deseas convertir: ");
+                    cantidadConversion = lectura.nextDouble();
+
+                    try{
+                        moneda = consultarMoneda.convertirMoneda(monedaBase,monedaConvertida,cantidadConversion);
+                        System.out.println("El valor de "+cantidadConversion+" "+moneda.base_code()+" equivalen a "
+                                + moneda.conversion_result()+" "+ moneda.target_code());
+                    }catch (RuntimeException e){
+                        System.out.println(e.getMessage());
+                        System.out.println("Finalizando programa!");
+                        opcion=7;
+                    }
+                    break;
+                case 7:
+                    System.out.println("Gracias por usar nuestro sistema de conversion de monedas. Hasta pronto");
+                    break;
+                default:
+                    System.out.println("Ingresa una opcion valida. Entre 1 y 7");
+                    break;
+            }
+        }
+
     }
 }
